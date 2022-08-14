fc56c1e1b2c56f0955d536ca9816c62016952f93
ghassan_mg <ghassan.mgsyr@gmail.com>
2022-08-14 15:58:43 +0300

    create first time articles view model ( refactor )

---
+++ DataLayer/ViewModels/FirstTimeArticlesViewModel.cs
@@ -0,0 +1,39 @@
+﻿using DataLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataLayer.ViewModels
+{
+    public class FirstTimeArticlesViewModel
+    {
+
+        [DisplayName("المنتج")]
+        public string Name { get; set; }
+        [DisplayName("نوع الفاتورة")]
+        public string InvoiceKind { get; set; }
+        [Browsable(false)]
+        public int UnitId { get; set; }
+        [NotMapped]
+        [DisplayName("الواحدة")]
+        public string UnitIdDescr
+        {
+            get
+            {
+                return DescriptionFK.GetUnitName(UnitId);
+            }
+            set {; }
+        }
+        [DisplayName("السعر")]
+        public double Price { get; set; }
+        [DisplayName("كمية أول المدة")]
+        public int Quantity { get; set; }
+        [DisplayName("الإجمالي")]
+        public double Total { get; set; }
+
+        [DisplayName("تاريخ ")]
+        public DateTime Expirydate { get; set; }
+    }
+}
